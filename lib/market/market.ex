d5156338b062f63f99932e8590028e137552ee62
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-05-04 22:07:27 +0200

    Implemented DdexFetcher with initial REST API fetch, followed by WS subscription for updates.
    * /exchanges API endpoint implemented for quickly fetching all exchanges in the market.

--- lib/market/market.ex
+++ lib/market/market.ex
@@ -14,6 +14,8 @@ defmodule Market do
         GenServer.call(__MODULE__, :get_market)
       :rebased_market ->
         GenServer.call(__MODULE__, :get_rebased_market)
+      :exchanges ->
+        GenServer.call(__MODULE__, :get_exchanges)
       _ ->
         nil
     end
@@ -48,6 +50,11 @@ defmodule Market do
     {:reply, format_market(rm), state}
   end
 
+  @impl true
+  def handle_call(:get_exchanges, _from, %{market: m} = state) do
+    {:reply, exchanges_in_market(m), state}
+  end
+
   defp format_market(m) do
     Map.values(m)
     |> Enum.sort_by(&combined_volume_across_exchanges/1, &>=/2)
@@ -59,57 +66,84 @@ defmodule Market do
     end)
   end
 
-  @impl true
-  def handle_cast({:update, exchange_market}, prev_state) do
-    {:noreply, merge(prev_state, exchange_market)}
+  def exchanges_in_market(market) do
+    Enum.reduce(market, MapSet.new(), fn ({_k, p}, acc1) ->
+      Enum.reduce(p.market_data, acc1, fn {exchange, _emd}, acc2->
+        MapSet.put(acc2, exchange)
+      end)
+    end)
   end
 
-  def merge(%{market: prev_market}, %ExchangeMarket{market: m}) do
-    market = Enum.reduce(m, prev_market, fn (p, acc) ->
-      %MarketFetching.Pair{
-        base_address: ba,
-        quote_address: qa,
-        base_symbol: bs,
-        quote_symbol: qs,
-        market_data: %MarketFetching.PairMarketData{
-          exchange: ex,
-          last_price: lp,
-          current_bid: cb,
-          current_ask: ca,
-          base_volume: bv,
-          quote_volume: qv
-        }
-      } = p
+  @impl true
+  def handle_cast({:update, %ExchangeMarket{} = em}, prev_state) do
+    {:noreply, merge(prev_state, em)}
+  end
 
-      id = pair_id(ba, qa)
-      emd = %Market.ExchangeMarketData{
+  @impl true
+  def handle_cast({:update, %MarketFetching.Pair{} = p}, prev_state) do
+    {:noreply, merge(prev_state, p)}
+  end
+
+  def add_pair(m, %MarketFetching.Pair{} = p) do
+    %MarketFetching.Pair{
+      base_address: ba,
+      quote_address: qa,
+      base_symbol: bs,
+      quote_symbol: qs,
+      market_data: %MarketFetching.PairMarketData{
+        exchange: ex,
         last_price: lp,
         current_bid: cb,
         current_ask: ca,
         base_volume: bv,
         quote_volume: qv
       }
+    } = p
 
-      market_entry =
-        case Map.has_key?(acc, id) do
-          false ->
-            %Market.Pair{
-              base_symbol: bs,
-              base_address: ba,
-              quote_address: qa,
-              quote_symbol: qs,
-              market_data: %{
-                ex => emd
-              }
+    id = pair_id(ba, qa)
+    emd = %Market.ExchangeMarketData{
+      last_price: lp,
+      current_bid: cb,
+      current_ask: ca,
+      base_volume: bv,
+      quote_volume: qv
+    }
+
+    market_entry =
+      case Map.has_key?(m, id) do
+        false ->
+          %Market.Pair{
+            base_symbol: bs,
+            base_address: ba,
+            quote_address: qa,
+            quote_symbol: qs,
+            market_data: %{
+              ex => emd
             }
-          true ->
-            %{acc[id] | market_data: Map.put(acc[id].market_data, ex, emd)}
-        end
-      Map.put(acc, id, market_entry)
+          }
+        true ->
+          %{m[id] | market_data: Map.put(m[id].market_data, ex, emd)}
+      end
+
+    Map.put(m, id, market_entry)
+  end
+
+  @dai_address "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359
+"
+  def merge(%{market: prev_market}, %MarketFetching.Pair{} = p) do
+    market = add_pair(prev_market, p)
+
+    rebased_market = Rebasing.rebase_market(@dai_address, market, 4)
+
+    %{market: market, rebased_market: rebased_market}
+  end
+
+  def merge(%{market: prev_market}, %ExchangeMarket{market: m}) do
+    market = Enum.reduce(m, prev_market, fn (p, acc) ->
+      add_pair(acc, p)
     end)
 
-    dai_address = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
-    rebased_market = Rebasing.rebase_market(dai_address, market, 4)
+    rebased_market = Rebasing.rebase_market(@dai_address, market, 4)
 
     %{market: market, rebased_market: rebased_market}
   end
