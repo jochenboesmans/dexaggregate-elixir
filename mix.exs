de73e44840d53a03b7ac9e462a21a1343a9b3d93
jochenboesmans <jochen.boesmans@gmail.com>
2019-05-06 08:27:44 +0200

    Added absinthe dependencies to mix.exs.

--- mix.exs
+++ mix.exs
@@ -23,11 +23,13 @@ defmodule DexAggregate.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~>1.5"},
-      {:poison, "~>4.0"},
-      {:websockex, "~>0.4.0"},
+      {:httpoison, "~> 1.5"},
+      {:poison, "~> 4.0"},
+      {:websockex, "~> 0.4.0"},
       {:plug_cowboy, "~> 2.0"},
       {:memoize, "~> 1.3"},
+	    {:absinthe, "~> 1.4"},
+	    {:absinthe_plug, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
