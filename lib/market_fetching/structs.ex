38b3f1621843fcd4f7ab4757e642eef22fab662c
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-06-06 17:41:31 +0200

    Added MarketFetching.Structs typespecs.

--- lib/market_fetching/structs.ex
+++ lib/market_fetching/structs.ex
@@ -1,45 +1,57 @@
 defmodule Dexaggregatex.MarketFetching.Structs do
 	@moduledoc """
-		Data structures for newly fetched market data.
+	Data structures for newly fetched market data.
 	"""
 
 	defmodule ExchangeMarket do
 		@moduledoc """
-			Data structure for newly fetched markets specific to an exchange.
+		Data structure for newly fetched markets specific to an exchange.
 		"""
 		@enforce_keys [:exchange, :market]
-		defstruct(
-			exchange: nil,
-			market: nil
-		)
+		defstruct [:exchange, :market]
+
+		@typedoc """
+		* exchange: atom representing exchange (i.e. :uniswap).
+		* market: list of pairs on the exchange.
+		"""
+		@type t :: %__MODULE__{exchange: atom(), market: [Pair]}
 	end
 
 	defmodule Pair do
 		@moduledoc """
-			Data structure for newly fetched data specific to a pair on an exchange.
+		Data structure for newly fetched data specific to a pair on an exchange.
 		"""
 		@enforce_keys [:base_symbol, :quote_symbol, :base_address, :quote_address, :market_data]
-		defstruct(
-			base_symbol: nil,
-			quote_symbol: nil,
-			quote_address: nil,
-			base_address: nil,
-			market_data: nil
-		)
+		defstruct [:base_symbol, :quote_symbol, :base_address, :quote_address, :market_data]
+
+		@typedoc """
+		* base_symbol: string representing the symbol of the base token.
+		* quote_symbol: string representing the symbol of the quote token.
+		* base_address: string representing the address of the base token.
+		* quote_address: string representing the address of the quote token.
+		* market_data: struct representing this pair's market data.
+		"""
+		@type t :: %__MODULE__{base_symbol: String.t(), quote_symbol: String.t(), base_address: String.t(),
+							 quote_address: String.t(), market_data: PairMarketData.t()}
 	end
 
 	defmodule PairMarketData do
 		@moduledoc """
-			Data structure for newly fetched market data specific to a pair on an exchange.
+		Data structure for newly fetched market data specific to a pair on an exchange.
+		All prices and volumes are denominated in the base token and are exchange-specific.
 		"""
 		@enforce_keys [:exchange, :last_price, :current_bid, :current_ask, :base_volume]
-		defstruct(
-			exchange: nil,
-			last_price: nil,
-			current_bid: nil,
-			current_ask: nil,
-			base_volume: nil
-		)
+		defstruct [:exchange, :last_price, :current_bid, :current_ask, :base_volume]
+
+		@typedoc """
+		* exchange: atom representing exchange (i.e. :uniswap).
+		* last_price: float representing the last price for the quote token.
+		* current_bid: float representing the highest current bid price for the quote token.
+		* current_ask: float representing the lowest current ask price for the quote token.
+		* base_volume: float representing a pair's volume.
+		"""
+		@type t :: %__MODULE__{exchange: atom(), last_price: float(),
+								 current_bid: float(), current_ask: float(), base_volume: float()}
 	end
 
 end
