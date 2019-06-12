eea72dc8fe59498e88a0f870b1f849049c3c0fbe
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-06-12 16:22:05 +0200

    Add Earmark, ExDoc deps.

--- mix.exs
+++ mix.exs
@@ -52,7 +52,9 @@ defmodule Dexaggregatex.MixProject do
 	    {:absinthe_plug, "~> 1.4"},
       {:absinthe_phoenix, "~> 1.4"},
       {:neuron, "~> 1.2.0"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:earmark, "~> 1.2", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: [:dev], runtime: false}
       # How to add deps (run "mix deps.get" afterwards):
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
